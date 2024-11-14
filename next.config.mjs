dd80f0a1158d9242c8b2923a9ccb897208fe0b70
128lvt <128vanthoai@gmail.com>
2024-11-14 19:35:21 +0700

    fix version

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+      domains: ['bombom.id.vn','localhost'], // Thêm 'localhost' vào đây
+    },
+};
+
+export default nextConfig;
--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
-  images: {
-    domains: ['https://206.189.42.99:8443', 'localhost'], // Thêm 'localhost' vào đây
-  },
-}
-
-export default nextConfig
